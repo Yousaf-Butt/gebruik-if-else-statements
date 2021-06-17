76a45e17762743be407c452f6948712a1772fb43
Yousaf <78146910+Yousaf-Butt@users.noreply.github.com>
2021-06-17 10:49:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18 && isFemale) {
+    console.log("Je mag naar binnen.")
+}
+else {
+    console.log("Jij komt er niet in.")
+}
+
+if (driverStatus === "bob") {
+    console.log("Jij bent de Bob dus je mag rijden.")
+}
+
+else {
+    console.log("Je mag vanavond niet meer achter het stuur zitten.")
+}
